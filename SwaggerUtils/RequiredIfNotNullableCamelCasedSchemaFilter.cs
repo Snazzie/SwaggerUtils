fc5dddaf6b489685bf2deafa65f8a51c9ac5d141
Aaron Cooper <snazzie@snazzie.space>
2023-08-17 16:03:06 +0100

    .

--- SwaggerUtils/RequiredIfNotNullableCamelCasedSchemaFilter.cs
+++ SwaggerUtils/RequiredIfNotNullableCamelCasedSchemaFilter.cs
@@ -6,30 +6,6 @@ namespace Snazzie.SwaggerUtils
 {
     /// <summary>
     /// Makes all non nullable properties required by default
-    /// Also forces camel case on generated property name
-    /// Usage: `options.SchemaFilter<SwaggerRequiredSchemaFilter>();`
-    /// </summary>
-    public class RequiredIfNotNullableCamelCasedSchemaFilter : ISchemaFilter
-    {
-        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
-        {
-            var properties = context.Type.GetProperties();
-            schema.Required ??= new HashSet<string>();
-
-            foreach (var property in properties)
-            {
-                var propertyNameInCamelCasing = char.ToLowerInvariant(property.Name[0]) + property.Name[1..];
-                var nullabilityContext = new NullabilityInfoContext();
-                var nullabilityInfo = nullabilityContext.Create(property);
-                if (nullabilityInfo.WriteState is not NullabilityState.Nullable)
-                    schema.Required.Add(propertyNameInCamelCasing);
-            }
-            schema.Nullable = false;
-        }
-    }
-
-    /// <summary>
-    /// Makes all non nullable properties required by default
     /// Usage: `options.SchemaFilter<SwaggerRequiredSchemaFilter>();`
     /// </summary>
     public class RequiredIfNotNullableSchemaFilter : ISchemaFilter
