fceee98d1baa906440349db2bcb9802a1e0c02ea
Aaron Cooper <snazzie@snazzie.space>
2023-08-17 15:42:53 +0100

    .

--- SwaggerUtils/RequiredIfNotNullableCamelCasedSchemaFilter.cs
+++ SwaggerUtils/RequiredIfNotNullableCamelCasedSchemaFilter.cs
@@ -6,9 +6,10 @@ namespace Snazzie.SwaggerUtils
 {
     /// <summary>
     /// Makes all non nullable properties required by default
+    /// Also forces camel case on generated property name
     /// Usage: `options.SchemaFilter<SwaggerRequiredSchemaFilter>();`
     /// </summary>
-    public class RequiredIfNotNullableSchemaFilter : ISchemaFilter
+    public class RequiredIfNotNullableCamelCasedSchemaFilter : ISchemaFilter
     {
         public void Apply(OpenApiSchema schema, SchemaFilterContext context)
         {
@@ -22,6 +23,28 @@ namespace Snazzie.SwaggerUtils
                 var nullabilityInfo = nullabilityContext.Create(property);
                 if (nullabilityInfo.WriteState is not NullabilityState.Nullable)
                     schema.Required.Add(propertyNameInCamelCasing);
+            }
+            schema.Nullable = false;
+        }
+    }
+
+    /// <summary>
+    /// Makes all non nullable properties required by default
+    /// Usage: `options.SchemaFilter<SwaggerRequiredSchemaFilter>();`
+    /// </summary>
+    public class RequiredIfNotNullableSchemaFilter : ISchemaFilter
+    {
+        public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+        {
+            var properties = context.Type.GetProperties();
+            schema.Required ??= new HashSet<string>();
+
+            foreach (var property in properties)
+            {
+                var nullabilityContext = new NullabilityInfoContext();
+                var nullabilityInfo = nullabilityContext.Create(property);
+                if (nullabilityInfo.WriteState is not NullabilityState.Nullable)
+                    schema.Required.Add(property.Name);
             }
             schema.Nullable = false;
         }
